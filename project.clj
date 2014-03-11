a0c25b3eb8ab4348245a8ce1f781b930fae1dc95
vagrant <vagrant@precise64.(none)>
2014-03-11 01:51:30 +0000

    Initial commit: new lein project with minimal customization

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject search-clojure-archives "0.0.1"
+  :description "Search the #clojure archives"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ src/search_clojure_archives/core.clj
@@ -0,0 +1,6 @@
+(ns search-clojure-archives.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
